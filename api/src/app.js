f75b400f45c7abd3e7e3ffa6374e49a23b6fa9cb
adriana <orochimei11@gmail.com>
2021-08-31 15:49:14 -0300

    todo listo para armar los controladores y rutas

--- api/src/app.js
+++ api/src/app.js
@@ -1,17 +1,9 @@
 const express = require('express');
 const morgan = require('morgan');
 const server = express();
-const mongoose = require('mongoose');
-const PORT = 3001;
 const routes = require('./routes/index');
 
-mongoose.connect('mongodb://localhost/requests')
-.then(db => {
-    console.log('DB connected');
-})
-.catch(err => {
-    console.log(err);
-});
+
 // dropea la collection igual q el {force: true}, pero aun no se donde iria XD.
 // mongoose.connection.db.dropCollection('products', function(err, results) {
 //     if(err) console.log(err);
@@ -19,11 +11,7 @@ mongoose.connect('mongodb://localhost/requests')
 // });
 
 server.use(morgan('dev'));
-server.use(express.urlencoded());
+//server.use(express.urlencoded());
 server.use(express.json())
 
 server.use('/', routes);
-
-server.listen(PORT, () => {
-    console.log('server listening on port: ', PORT);
-});
