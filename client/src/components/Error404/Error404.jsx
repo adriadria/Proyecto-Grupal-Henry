a365b7a8456313cb767ff747edf3cd9a5849ad84
Keltuzad29 <c.cerquera.123@gmail.com>
2021-09-01 10:04:23 -0500

    Borrar

--- client/src/components/Error404/Error404.jsx
+++ client/src/components/Error404/Error404.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Error404() {
     return (
         <div>
-            Soy el Error 404 :(
+            Soy el Error 404 😭
         </div>
     )
 }
