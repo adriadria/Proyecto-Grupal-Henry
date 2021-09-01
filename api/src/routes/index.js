f62eb8e5cb6d543d45daaf62448dc2c47b6e74b2
facu <facu-figueroa-98@hotmail.com>
2021-09-01 00:11:27 -0300

    add getProducts and postProduct routes

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,12 +1,32 @@
 const { Router } = require('express');
+const Product = require('../models/Product');
+const {getProducts} = require('../controllers');
 
 const router = Router();
-
-const Product = require('../models/Product');
 
 router.get('/', (req, res) => {
     res.status(200).json({msg: 'it works!'});
 });
+
+router.get('/products', async (req, res) => {
+    try{
+        const products = await getProducts();
+        return res.status(200).send(products);
+    }catch(e){
+        console.error(e);
+    }
+});
+
+router.post('/product', async (req, res) => {
+    const {name, price, description, image, weight, height, width, stock} = req.body;
+    try{
+        await new Product({name, price, description, image, weight, height, width, stock});
+        return res.status(200).send('Product successfully created')
+    }catch(e){
+        console.error(e);
+    }
+});
+
 router.post('/', async(req, res) => {
     console.log(req.body);
     const product = new Product(req.body);
