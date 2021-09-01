eb7f44ceaf1fcf2e26801348f2af06be5b083a14
facu <facu-figueroa-98@hotmail.com>
2021-09-01 11:17:58 -0300

    add getCategories and postCategory routes

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,6 +1,7 @@
 const { Router } = require('express');
 const Product = require('../models/Product');
-const {getProducts} = require('../controllers');
+const Category = require('../models/Category');
+const {getProducts, getCategories} = require('../controllers');
 
 const router = Router();
 
@@ -20,19 +21,40 @@ router.get('/products', async (req, res) => {
 router.post('/product', async (req, res) => {
     const {name, price, description, image, weight, height, width, stock} = req.body;
     try{
-        await new Product({name, price, description, image, weight, height, width, stock});
-        return res.status(200).send('Product successfully created')
+        const product = new Product({name, price, description, image, weight, height, width, stock});
+        await product.save();
+        return res.status(200).send('Product successfully created');
     }catch(e){
         console.error(e);
     }
 });
 
-router.post('/', async(req, res) => {
-    console.log(req.body);
-    const product = new Product(req.body);
-    console.log(product);
-    await product.save();
-    res.status(200).json(product);
+router.get('/categories', async (req, res) => {
+    try{
+        const categories = await getCategories();
+        return res.status(200).send(categories);
+    }catch(e){
+        console.error(e);
+    }
+});
+
+router.post('/category', async (req, res) => {
+    const {name} = req.body;
+    try{
+        const category = new Category({name});
+        await category.save();
+        return res.status(200).send(`${name} category successfully created`);
+    }catch(e){
+        console.error(e);
+    }
 })
+
+// router.post('/', async(req, res) => {
+//     console.log(req.body);
+//     const product = new Product(req.body);
+//     console.log(product);
+//     await product.save();
+//     res.status(200).json(product);
+// })
 
 module.exports =  router;
