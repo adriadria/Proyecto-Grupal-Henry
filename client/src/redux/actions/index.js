9b16d555dca1169bbcfb3d99ebad38a0d2c79bab
estebanchoque <choqueestebanjoaquin@gmail.com>
2021-09-02 15:50:45 -0300

    add index constants file

--- client/src/redux/actions/index.js
+++ client/src/redux/actions/index.js
@@ -1,50 +1,52 @@
-import axios from 'axios';
-
-export const GET_CATEGORIES = 'GET_CATEGORIES', GET_PRODUCTS = 'GET_PRODUCTS', GET_PRODUCTS_BY_NAME = 'GET_PRODUCTS_BY_NAME';
+import axios from "axios";
+import types from "../constants/types";
+const BASE_URL = "http://localhost:3001";
 
 // http://localhost:3001/categories GET
 // http://localhost:3001/categories?name=... GET
 // http://localhost:3001/products GET
 
-// ------------- //
+// Products
 export const getProducts = () => {
-    return async function (dispatch) {
-        try {
-            const res = await axios.get(`http://localhost:3001/products`);
-            dispatch({
-                type: GET_PRODUCTS,
-                payload: res.data,
-            });
-        } catch (err) {
-            console.log(err);
-        }
+  return async (dispatch) => {
+    try {
+      const { data } = await axios.get(`${BASE_URL}/products`);
+      return dispatch({
+        type: types.GET_PRODUCTS,
+        payload: data,
+      });
+    } catch (err) {
+      console.log(err);
     }
+  };
 };
-export const getProductsByName = (name) => { // buscador --> desk? escritorio?
-    return async function (dispatch) {
-        try {
-            const res = await axios.get(`http://localhost:3001/categories?name=${name}`);
-            dispatch({
-                type: GET_PRODUCTS_BY_NAME,
-                payload: res.data,
-            });
-        } catch (err) {
-            console.log(err);
-            return dispatch({type: GET_PRODUCTS_BY_NAME, payload: []});
-        }
+
+export const getProductsByName = (name) => {
+  // buscador --> desk? escritorio?
+  return async (dispatch) => {
+    try {
+      const { data } = await axios.get(`${BASE_URL}/categories?name=${name}`);
+      return dispatch({
+        type: types.GET_PRODUCTS_BY_NAME,
+        payload: data,
+      });
+    } catch (err) {
+      console.log(err);
     }
+  };
 };
-// ------------- //
+
+// Categories
 export const getCategories = () => {
-    return async function (dispatch) {
-        try {
-            const res = await axios.get(`http://localhost:3001/categories`);
-            dispatch({
-                type: GET_CATEGORIES,
-                payload: res.data,
-            });
-        } catch (err) {
-            console.log(err);
-        }
+  return async (dispatch) => {
+    try {
+      const { data } = await axios.get(`http://localhost:3001/categories`);
+      return dispatch({
+        type: types.GET_CATEGORIES,
+        payload: data,
+      });
+    } catch (err) {
+      console.log(err);
     }
+  };
 };
