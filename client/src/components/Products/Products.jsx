54ce7705c7945efef057c802445d249ce207fd07
Alejandro Flores <sjafs.83@gmail.com>
2021-09-02 13:19:32 -0300

    traer al front info de product del back

--- client/src/components/Products/Products.jsx
+++ client/src/components/Products/Products.jsx
@@ -1,15 +1,38 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { getProducts } from '../../redux/actions';
 import Product from '../Product/Product';
 import prodsStyle from './Products.module.css';
 
 
+
 const Products = () => {
+    var productsArr = useSelector(state => state.products);
+    const dispatch = useDispatch();
+    console.log(productsArr);
+
+    useEffect(() => {
+		dispatch(getProducts()); 
+	}, [dispatch]);
+
+
 	return (
 		<>
             <div className={prodsStyle.title1}>
                 Produts List Catalog
             </div>
 			<div className={prodsStyle.contenedor}>
+                {
+                    productsArr.map(e => (
+                        <Product 
+                            key={e._id}
+                            id={e._id}
+                            image={e.image_url}
+                            name={e.name}R
+                        />
+                    ))
+                }
+
                 <Product 
                     id={1} 
                     image={'https://medias.maisonsdumonde.com/image/upload/q_auto,f_auto/w_2000/img/escritorio-de-metal-blanco-y-abeto-con-1-cajon-y-1-puerta-1000-4-2-192784_1.jpg'}
