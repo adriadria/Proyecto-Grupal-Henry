bea4e5d2a1676927ee075b088da28555ed04b3c2
BusquetsLA <busquetsla@gmail.com>
2021-09-02 11:39:38 -0300

    agregado GET_PRODUCTS_BY_NAME al reducer

--- client/src/redux/reducer/index.js
+++ client/src/redux/reducer/index.js
@@ -1,4 +1,4 @@
-import { GET_PRODUCTS, GET_CATEGORIES } from '../actions/index';
+import { GET_PRODUCTS, GET_CATEGORIES, GET_PRODUCTS_BY_NAME } from '../actions/index';
 
 const initialState = {
   products: [], // todos los products cargados de getProducts
@@ -9,6 +9,11 @@ const initialState = {
 const rootReducer = (state = initialState, action) => {
   switch (action.type) {
     case GET_PRODUCTS:
+      return {
+        ...state,
+        products: action.payload,
+      }
+    case GET_PRODUCTS_BY_NAME:
       return {
         ...state,
         products: action.payload,
