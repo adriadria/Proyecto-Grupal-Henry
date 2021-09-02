72d6b9f93f8e798d35d9de87c5e2ec42ce571a96
Alejandro Flores <sjafs.83@gmail.com>
2021-09-02 19:50:48 -0300

    trabajando en componente detail

--- client/src/components/Detail/Detail.jsx
+++ client/src/components/Detail/Detail.jsx
@@ -1,16 +1,27 @@
-import React from 'react'
-import NavBar from '../NavBar/NavBar'
-import Product from '../Product/Product'
+import React from 'react';
+import NavBar from '../NavBar/NavBar';
+//import Product from '../Product/Product'
+import detStyle from './Detail.module.css';
 
 export default function Detail() {
-    return (
-        <div>
-            <NavBar />
-            <Product 
+	return (
+		<div>
+			<NavBar />
+			<div className={detStyle.content}>
+				<div className={detStyle.info1}>
+                    <img src="https://medias.maisonsdumonde.com/image/upload/q_auto,f_auto/w_2000/img/escritorio-de-metal-blanco-y-abeto-con-1-cajon-y-1-puerta-1000-4-2-192784_1.jpg" 
+                    alt="product"/>
+                </div>
+                <div className={detStyle.info2}>
+                    Detalles
+                </div>
+			</div>
+
+			{/* <Product 
                     id={1} 
                     image={'https://medias.maisonsdumonde.com/image/upload/q_auto,f_auto/w_2000/img/escritorio-de-metal-blanco-y-abeto-con-1-cajon-y-1-puerta-1000-4-2-192784_1.jpg'}
                     name={'SUNSET'}
-                    />
-        </div>
-    )
+                    /> */}
+		</div>
+	);
 }
