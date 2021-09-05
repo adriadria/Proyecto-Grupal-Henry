8f2207ece66226ded04af70b7059b2796421d805
Keltuzad29 <c.cerquera.123@gmail.com>
2021-09-05 08:06:39 -0500

    se agrega option para ordenamiento por precio

--- client/src/components/NavBar/NavBar.jsx
+++ client/src/components/NavBar/NavBar.jsx
@@ -19,18 +19,16 @@ const NavBar = () => {
     dispatch(getCategories());
   }, [dispatch]);
 
-
   function handleClick(e) {
     //e.preventDefault();
     dispatch(getProducts());
   }
 
-
   function handleFilterCategories(e) {
     dispatch(filterByCategory(e.target.value));
   }
-  let rangos=["0-500","501-1000","1001-1500","1501-2000","2001-2500"];
- 
+  let rangos = ["0-500", "501-1000", "1001-1500", "1501-2000", "2001-2500"];
+
   return (
     <nav>
       <div className={styles.container}>
@@ -41,7 +39,9 @@ const NavBar = () => {
             className={styles.select}
             onChange={(e) => handleFilterCategories(e)}
           >
-              <option value="all_categories" key="0">Categorias Todas</option>
+            <option value="all_categories" key="0">
+              Categorias Todas
+            </option>
             {categories.map((cat) => (
               <option value={cat._id} key={cat._id}>
                 {cat.name}
@@ -52,17 +52,35 @@ const NavBar = () => {
             className={styles.select}
             //onChange={(e) => handleFilterCategories(e)}
           >
-              <option value="rango" key="0">Precios</option>
-            {rangos.map((rango,index) => (
-              <option key={index}>
-                {rango}
-              </option>
+            <option value="rango" key="0">
+              Precios
+            </option>
+            {rangos.map((rango, index) => (
+              <option key={index}>{rango}</option>
             ))}
+          </select>
+          <select
+            className={styles.select}
+            //onChange={(e) => handleFilterCategories(e)}
+          >
+            <option value="all" key="0">
+              Ordenar por Precio
+            </option>
+            <option value="ascendente" key="1">
+              Menor a Mayor
+            </option>
+            <option value="descendente" key="2">
+              Mayor a Menor
+            </option>
           </select>
         </div>
         <div className={styles.container_links}>
           <div className={styles.links}>
-            <NavLink className={styles.link_text} to="/" onClick={(e) => handleClick(e)} >
+            <NavLink
+              className={styles.link_text}
+              to="/"
+              onClick={(e) => handleClick(e)}
+            >
               Inicio
             </NavLink>
             <NavLink className={styles.link_text} to="/about">
