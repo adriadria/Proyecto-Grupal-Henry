f1245aac3b9b051ace8924d769ed1ef913b52bc0
Alejandro Flores <sjafs.83@gmail.com>
2021-09-05 08:39:57 -0300

    se muestra la categoria de cada producto y se añadio un coponente si una busqueda o filtro no encuentra info

--- client/src/components/Product/Product.jsx
+++ client/src/components/Product/Product.jsx
@@ -1,43 +1,41 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 import prodStyle from './Product.module.css';
 
 const Product = ({ id, image, name, price, catArr }) => {
+	var categories = useSelector((state) => state.categories);
+
+	function DisplayNameCategory(e) {
+		let found = categories.find((c) => {
+			return c._id === e;
+		});
+		return found.name;
+	}
 
 	return (
 		<Link to={`/detail/${id}`}>
-			{/* 
-			<div className={prodStyle.card}>
-				<img src={image} alt="imagen" />
-				<div className={prodStyle.cardData1}>
-					<span className={prodStyle.cardPrice}>Price: ${price}</span>
-					<span>💎💎</span>
-				</div>
-				<div className={prodStyle.cardData2}>
-					<div className={prodStyle.cardText}>{name}</div>
-				</div>
-			</div>
-			 */}
-
 			<div className={prodStyle.card1}>
 				<div className={prodStyle.cardheader}>
 					<img src={image} alt={name} />
 				</div>
 				<div className={prodStyle.cardbody}>
-			 		{
-						catArr.map(e => (
-							<div className={prodStyle.tag} key={e}>{
-								e
+					<div className={prodStyle.cardCagegories}>
+						{catArr.map((e) => (
+							<span className={prodStyle.tag} key={e}>
+								{DisplayNameCategory(e)}
+							</span>
+						))}
+					</div>
 
-							}</div>
-						))
-					 }
 					{/* <div className={prodStyle.tag}>General</div> */}
 					<div className={prodStyle.cardData1}>
-					<span className={prodStyle.cardPrice}><b>Price:</b> ${price}</span>
-					<span>💎💎</span>
-				</div>
-				<div className={prodStyle.cardText}>{name}</div>
+						<span className={prodStyle.cardPrice}>
+							<b>Price:</b> ${price}
+						</span>
+						<span>💎💎</span>
+					</div>
+					<div className={prodStyle.cardText}>{name}</div>
 				</div>
 			</div>
 		</Link>
